f103c8581c670ece7cee13a1005ac55bafd3b286
Jezler <146593042+oJezler-git@users.noreply.github.com>
2026-04-03 20:48:46 +0100

    feat(client): integrate virtual keyboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,8 @@ import { ThemeProvider } from './context/ThemeContext';
 import { CallerProvider } from './context/CallerContext';
 import { PosDashboard } from './components/layout/pos-dashboard';
 
+import { VirtualKeyboard } from './components/ui/virtual-keyboard';
+
 function App() {
   return (
     <ThemeProvider>
@@ -18,6 +20,7 @@ function App() {
           </OrderProvider>
         </UIProvider>
       </CallerProvider>
+      <VirtualKeyboard />
     </ThemeProvider>
   );
 }
